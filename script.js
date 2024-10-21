982acf117e73c88458077b04de7eb37fce847fe0
Lev <90378065+usernameLev@users.noreply.github.com>
2024-10-21 11:42:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-document.getElementById('startButton').addEventListener('click', function() {
-  const sound = document.getElementById('sound');
-  
+const sound = document.getElementById('sound');
+let intervalId; // Переменная для хранения идентификатора таймера
+
+document.getElementById('startButton').addEventListener('click', function () {
   // Функция для генерации случайного времени
   function getRandomTime(min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
   // Генерация звука через случайные интервалы
   function playSound() {
-      sound.play();
-      const randomInterval = getRandomTime(1000, 5000); // от 1 до 5 секунд
-      setTimeout(playSound, randomInterval);
+    sound.play();
+    const randomInterval = getRandomTime(1000, 5000); // от 1 до 5 секунд
+    intervalId = setTimeout(playSound, randomInterval);
   }
 
   playSound(); // Запуск генерации звуков
+});
+
+document.getElementById('stopButton').addEventListener('click', function () {
+  clearTimeout(intervalId); // Остановка генерации звуков
 });
